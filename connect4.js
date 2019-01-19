5e41ed7bfa9dd764a4f34444315b8d0bb6b32341
Isaac Leanos <isaac.leanos11@gmail.com>
2019-01-19 15:04:47 -0800

    hover effect for user's options or potential cell placement

--- connect4.js
+++ connect4.js
@@ -8,21 +8,57 @@ class Connect4{
         this.columns=7;
         this.selector=selector;
         this.createGrid();        
+        this.setEventListeners();
     }
 
-    createGrid(){
-
+    createGrid()
+    {
         const $board=$(this.selector);
         for(let row=0; row<this.rows; row++){
-                const $row=$('<div>').addClass('row');
-            for(let col=0; col<this.columns; col++){
-                const $col=$('<div>').addClass('column empty');
-                $row.append($col);
+            const $row=$('<div>').addClass('row');
+                for(let col=0; col<this.columns; col++){
+                    const $col=$('<div>').addClass('column empty').attr('data-col',col).attr('data-row',row);
+                    $row.append($col);
                 }
             $board.append($row);
             }
     }
 
+    setEventListeners()
+    {
+        const $board=$(this.selector);
+
+        function findLastEmptyCell(col)
+        {
+            const cells=$(`.column[data-col='${col}']`);
+            console.log('cells',cells);
+            // length = 5
+            for(let i=cells.length-1; i>=0; i--){
+                const $cell=$(cells[i]);
+                if($cell.hasClass('empty') ){
+                    return $cell;
+                }
+            }
+            return null;
+        }
+
+        // hover over col that has both col and empty class
+        $board.on('mouseenter','.column.empty',function(e)
+        {
+            console.log('cell',this);
+            const col=$(this).data('col');
+            console.log('data-col',col);
+
+            const $lastEmptyCell=findLastEmptyCell(col);
+            $lastEmptyCell.addClass('next-red');
+        });
+
+        $board.on('mouseleave','.column',function()
+        {
+            $('.column').removeClass('next-red');
+        });
+
+    }
 
 
 
